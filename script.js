2038d26e93a05f5a4fe807a1ff637148efb373c9
YetiHafen <yetihafen734@gmail.com>
2021-06-16 14:22:07 +0200

    setup page

---
+++ script.js
@@ -0,0 +1,7 @@
+/*let items = document.getElementsByClassName("menu-item");
+let i = 0;
+for(let item in items) {
+    item.style.left = (4 + i*2) + "em;";
+    console.log(item + " updated to left:" + (4 + i*2) + "em;");
+    i++;
+}*/
